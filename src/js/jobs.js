1be161bb43a0e4c1d14da08d1f4313ec5c686ff3
Sebastian <luszczek.sebastian@awesome-ind.com>
2019-03-12 10:25:34 +0100

    + job modals

---
+++ src/js/jobs.js
@@ -0,0 +1,33 @@
+export default [{
+        "position": "C# Programmer",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        "salary": "3000 - 4000 zł",
+        "requirements": [
+            "miły", "ładny", "cichy"
+        ]
+    },
+    {
+        "position": "3D Modeller",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        "salary": "3000 - 4000 zł",
+        "requirements": [
+            "miły", "ładny", "cichy"
+        ]
+    },
+    {
+        "position": "Front-End Dev",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        "salary": "3000 - 4000 zł",
+        "requirements": [
+            "miły", "ładny", "cichy"
+        ]
+    },
+    {
+        "position": "Back-End Dev",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        "salary": "3000 - 4000 zł",
+        "requirements": [
+            "miły", "ładny", "cichy"
+        ]
+    }
+]
--- src/js/main.js
+++ src/js/main.js
@@ -1,131 +1,157 @@
-import 'jquery';
-import 'jquery-scrollify';
-import '../scss/main.scss';
+import "jquery";
+import "jquery-scrollify";
+import "../scss/main.scss";
+import jobs from './jobs';
 
-document.addEventListener('DOMContentLoaded', function () {
-
+document.addEventListener("DOMContentLoaded", function () {
     let st = document.documentElement.scrollTop;
     let vh = Math.floor(window.innerHeight * 0.9);
 
-    // job modal
+    // Get the modal
+    const modal = document.getElementById("myModal");
 
-    const job_info_modal = document.querySelector('.job-modal');
-    const job_info_modal_header = document.querySelector('.job-modal #modal_header');
-    const job_info_modal_text = document.querySelector('.job-modal #modal_text');
-
-    const info_btns = document.querySelectorAll('.box-btns .info');
-
-    info_btns.forEach(btn => {
-        btn.addEventListener('click', () => {
-            job_info_modal.style.display = "block";
-
-            job_info_modal_header.innerText = btn.parentNode.parentNode.childNodes[1].innerText;
-            job_info_modal_text.innerHTML = `${btn.parentNode.parentNode.childNodes[3].innerHTML}`;
+    // Get the button that opens the modal
+    const job_btns = document.querySelectorAll(".job_btn");
+    job_btns.forEach((btn, index) => {
+        let ul = document.createElement('ul');
+        jobs[index].requirements.forEach(job => {
+            let li = document.createElement('li');
+            li.textContent = job;
+            ul.appendChild(li);
         })
+
+        btn.addEventListener("click", () => {
+            modal.style.display = "block";
+            modal.children[0].children[1].textContent = jobs[index].position;
+            modal.children[0].children[2].textContent = jobs[index].description;
+            modal.children[0].appendChild(ul);
+        });
     });
 
-    // document.querySelector('.close-btn-job').addEventListener('click', () => {
-    //     job_info_modal.style.display = "none";
-    // });
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function () {
+        modal.style.display = "none";
+        while (modal.children[0].children[3]) {
+            modal.children[0].removeChild(modal.children[0].children[3])
+        }
+    };
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    };
+
+    const job_boxes = document.querySelectorAll('.box');
+    job_boxes.forEach((box, index) => {
+        box.children[0].textContent = jobs[index].position;
+        box.children[1].textContent = jobs[index].description.slice(0, 40) + '...';
+    })
 
     // scrollify
 
     $(function () {
         $.scrollify({
             section: ".section",
-            sectionName: 'section',
+            sectionName: "section",
             scrollSpeed: 1000,
             overflowScroll: true,
             scrollbars: false,
             // easing: 'linear',
             before: function (i, panels) {
                 nav_bar.forEach(btn => {
-                    btn.classList.remove('active');
+                    btn.classList.remove("active");
                     if (btn.classList.contains(panels[i][0].dataset.section)) {
-                        btn.classList.add('active');
+                        btn.classList.add("active");
                     }
                 });
 
                 nav_bar_inn.forEach(btn => {
-                    btn.classList.remove('active');
+                    btn.classList.remove("active");
                     if (btn.classList.contains(panels[i][0].dataset.section)) {
-                        btn.classList.add('active');
+                        btn.classList.add("active");
                     }
                 });
             }
         });
     });
 
-    const nav_bar = document.querySelectorAll('.nav-btn');
-    const nav_bar_inn = document.querySelectorAll('.nav-btn-inn');
+    const nav_bar = document.querySelectorAll(".nav-btn");
+    const nav_bar_inn = document.querySelectorAll(".nav-btn-inn");
 
     nav_bar.forEach(btn => {
-        btn.addEventListener('click', function () {
+        btn.addEventListener("click", function () {
             let looked_class = btn.classList[0];
             $.scrollify.move(`#${looked_class}`);
             nav_bar.forEach(btn => {
-                btn.classList.remove('active');
+                btn.classList.remove("active");
                 if (btn.classList.contains(looked_class)) {
-                    btn.classList.add('active');
+                    btn.classList.add("active");
                 }
-                side_nav.classList.remove('active');
-                side_nav_icon.classList.remove('active');
+                side_nav.classList.remove("active");
+                side_nav_icon.classList.remove("active");
             });
-
-        })
+        });
     });
 
     nav_bar_inn.forEach(btn => {
-        btn.addEventListener('click', function () {
+        btn.addEventListener("click", function () {
             let looked_class = btn.classList[0];
             $.scrollify.move(`#${looked_class}`);
             nav_bar.forEach(btn => {
-                btn.classList.remove('active');
+                btn.classList.remove("active");
                 if (btn.classList.contains(looked_class)) {
-                    btn.classList.add('active');
+                    btn.classList.add("active");
                 }
             });
-
-        })
+        });
     });
 
-    if (document.URL.indexOf('what') == -1) {
-        document.querySelector('#contact_btn').addEventListener('click', () => {
+    if (document.URL.indexOf("what") == -1) {
+        document.querySelector("#contact_btn").addEventListener("click", () => {
             $.scrollify.move(`#cont`);
-        })
-    };
+        });
+    }
 
-    document.querySelector('.nav-logo').addEventListener('click', () => {
+    document.querySelector(".nav-logo").addEventListener("click", () => {
         $.scrollify.move(`#who`);
-    })
+    });
 
-    document.querySelectorAll('.next_section').forEach(btn => {
-        btn.addEventListener('click', () => {
+    document.querySelectorAll(".next_section").forEach(btn => {
+        btn.addEventListener("click", () => {
             $.scrollify.next();
         });
     });
 
-    document.querySelectorAll('.prev_section').forEach(btn => {
-        btn.addEventListener('click', () => {
+    document.querySelectorAll(".prev_section").forEach(btn => {
+        btn.addEventListener("click", () => {
             $.scrollify.previous();
         });
     });
 
     // side nav on mobile
-    const side_nav = document.querySelector('.side_nav');
-    const side_nav_icon = document.querySelector('.side_nav_icon');
-    side_nav_icon.addEventListener('click', sideNavToggle);
+    const side_nav = document.querySelector(".side_nav");
+    const side_nav_icon = document.querySelector(".side_nav_icon");
+    side_nav_icon.addEventListener("click", sideNavToggle);
 
     function sideNavToggle() {
-        this.classList.toggle('active');
-        if (side_nav.classList.contains('active')) {
-            side_nav.classList.remove('active');
+        this.classList.toggle("active");
+        if (side_nav.classList.contains("active")) {
+            side_nav.classList.remove("active");
         } else {
-            side_nav.classList.add('active');
+            side_nav.classList.add("active");
         }
-    };
+    }
 });
-document.querySelector('.res-monitor').innerText = `Width: ${window.innerWidth}px, height: ${window.innerHeight}px.`;
-window.addEventListener('resize', () => {
-    document.querySelector('.res-monitor').innerText = `Width: ${window.innerWidth}px, height: ${window.innerHeight}px.`;
-})
+document.querySelector(".res-monitor").innerText = `Width: ${
+  window.innerWidth
+}px, height: ${window.innerHeight}px.`;
+window.addEventListener("resize", () => {
+    document.querySelector(".res-monitor").innerText = `Width: ${
+    window.innerWidth
+  }px, height: ${window.innerHeight}px.`;
+});
